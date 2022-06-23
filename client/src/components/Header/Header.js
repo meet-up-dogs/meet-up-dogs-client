cded9b98ce9520282d85fa7130b48a2a27062bc6
Karol Wasemann <kw.kwaq@gmail.com>
2022-06-23 11:14:49 +0200

    added Header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,101 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+
+import Typography from "@mui/material/Typography";
+
+import IconButton from "@mui/material/IconButton";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
+import Switch from "@mui/material/Switch";
+
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+
+import Menu from "@mui/material/Menu";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Box from "@mui/material/Box";
+
+export default function Header(props) {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={props.login}
+                onChange={props.handleChange}
+                aria-label="login switch"
+              />
+            }
+            label={props.login ? "Logout" : "props.Login"}
+          />
+        </FormGroup>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {`Hello ${props.userName}`}
+            </Typography>
+            {props.login && (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  //   onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  //   anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  //   open={Boolean(anchorEl)}
+                >
+                  <MenuItem>Profile</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box
+        name="username"
+        label="Username"
+        variant="standard"
+        sx={props.margin}
+        // value={props.inputSignUp.username}
+        // onChange={(e) =>
+        //   props.setInputSignUp({
+        //     ...props.inputSignUp,
+        //     username: e.target.value,
+        //   })
+        // }
+      />
+    </div>
+  );
+}
--- client/src/components/userPofil/UserProfil.js
+++ client/src/components/userPofil/UserProfil.js
@@ -1,17 +1,7 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -23,12 +13,14 @@ import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import axios from "axios";
 import Map from "../Map/Map";
+import Header from "../Header/Header";
+import Box from "@mui/material/Box";
 
 // import {useToggle} from "../hooks/useToggle"
 
 export default function UserProfil(props) {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [login, setLogin] = React.useState(true);
+  // const [anchorEl, setAnchorEl] = React.useState(null);
 
   const [bottomLeft, setBottomLeft] = useState({});
   const [topRight, setTopRight] = useState({});
@@ -54,16 +46,16 @@ export default function UserProfil(props) {
   });
 
   const handleChange = (event) => {
-    setAuth(event.target.checked);
+    setLogin(event.target.checked);
   };
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // const handleMenu = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
 
   const userProfilHandler = async (e) => {
     console.log(userProfil);
@@ -97,82 +89,12 @@ export default function UserProfil(props) {
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={auth}
-                onChange={handleChange}
-                aria-label="login switch"
-              />
-            }
-            label={auth ? "Logout" : "Login"}
-          />
-        </FormGroup>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              {`Hello ${props.userName}`}
-            </Typography>
-            {auth && (
-              <div>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Box
-        name="username"
-        label="Username"
-        variant="standard"
-        sx={margin}
-        // value={props.inputSignUp.username}
-        // onChange={(e) =>
-        //   props.setInputSignUp({
-        //     ...props.inputSignUp,
-        //     username: e.target.value,
-        //   })
-        // }
+      <Header
+        login={login}
+        handleChange={handleChange}
+        margin={margin}
+        userName={props.userName}
       />
-
       <form
         action="/userprofil"
         method="post"
