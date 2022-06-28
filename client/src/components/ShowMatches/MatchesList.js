24a796315b3738f2a10ed34a0299616269c5a6cd
EveKlieber <Kliebereveline@gmail.com>
2022-06-28 14:14:50 +0200

    MatchList header and footer added

--- client/src/components/ShowMatches/MatchesList.js
+++ client/src/components/ShowMatches/MatchesList.js
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+import Header from "../Header/Header.js"
+import Footer from '../Footer/Footer';
 
 
 const MatchList  = () => {
   return (
     <>
+        <Header />
       <h2>Match List - x Treffer:</h2>
       <p>nnn</p>
+      <Footer />
     </>
   );
 };
